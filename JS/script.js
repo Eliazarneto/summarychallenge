7ec6d37ab874f9c8768f0927a5521dc760ed4bf8
Eliazarneto <129124838+Eliazarneto@users.noreply.github.com>
2023-05-25 19:31:46 -0300

    att 2

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,3 @@
-/*window.alert('Vamos iniciar com a digitação das notas nos prompts a seguir')
-var reação = Number.parseFloat(window.prompt('Digite a nota de reação (0 a 100): '))
-var memoria = Number.parseFloat(window.prompt('Digite a nota de memoria (0 a 100): '))
-var comunicação = Number.parseFloat(window.prompt('Digite a nota de comunicação (0 a 100): '))
-var visual = Number.parseFloat(window.prompt('Digite a nota de visual (0 a 100): '))
-
-/*var reação = window.prompt('Digite a nota de reação: ')
-var memoria = window.prompt('Digite a nota de Memória: ')
-var comunicação = window.prompt('Digite a nota de Comunicação: ')
-var visual = window.prompt('Digite a nota de Visual: ')*/
 
 function calcular() {
 
@@ -24,7 +14,7 @@ function calcular() {
 
     var digit = document.getElementById('btndigitar')
 
-    if(digit.style.display == 'block') {
+    if(digit.style.display === 'block') {
         digit.style.display = 'none'
     } else {
         digit.style.display = 'block'
@@ -62,30 +52,6 @@ function Digitarnotas() {
     }
  }      
 
-
-/*var nota_rea = document.getElementById('nota-reação')
-var nota_mem = document.getElementById('nota-memoria')
-var nota_com = document.getElementById('nota-comunicação')
-var nota_vis = document.getElementById('nota-visual')
-var comentario = document.getElementById('comentario')
-
-var nota_rec = document.getElementById('nota-recebida')
-var nota_final = (reação + memoria + comunicação + visual) / 4
-
-nota_rea.innerHTML = `${reação}`
-nota_mem.innerHTML = `${memoria}`
-nota_com.innerHTML = `${comunicação}`
-nota_vis.innerHTML = `${visual}`
-
-nota_rec.innerHTML = `${Math.round(nota_final)}`
-
-if (nota_final <= 40) {
-    comentario.innerHTML = `Ruim`
-} else if (nota_final <= 65){
-    comentario.innerHTML = `Bom`
-} else {
-    comentario.innerHTML = `Ótimo`
-}*/
 
 
 
